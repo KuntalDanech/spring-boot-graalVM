14aced34507a7291b39200bf840de8eb37edfdcf
Kuntal Danech <kuntal7398@gmail.com>
2022-04-06 11:25:12 +0530

    Spring boot graal vm - native image has been added

---
+++ src/test/java/com/danech/GraalVmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalVmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
